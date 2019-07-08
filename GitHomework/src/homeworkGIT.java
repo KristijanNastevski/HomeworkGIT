525c2ca87dec4318025854475808832e1d85561b
Kristijan Nastevski <Kristijan.Nastevski@endava.com>
2019-07-08 12:57:55 +0200

    Commited 2nd changes

--- GitHomework/src/homeworkGIT.java
+++ GitHomework/src/homeworkGIT.java
@@ -2,9 +2,9 @@ public class homeworkGIT {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World - Commited 1st change");
-        System.out.println("Hello Endava - Commited 1st change");
-        System.out.println("Hello Beko - Commited 1st change");
+        System.out.println("Hello World - Commited 2nd change");
+        System.out.println("Hello Endava - Commited 2nd change");
+        System.out.println("Hello Beko - Commited 2nd change");
 
     }
 }
