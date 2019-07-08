7a0836c9cb2d2cd873b6df4022804d823e2b6c3e
Kristijan Nastevski <Kristijan.Nastevski@endava.com>
2019-07-08 12:55:32 +0200

    Added the .java file

--- GitHomework/src/homeworkGIT.java
+++ GitHomework/src/homeworkGIT.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class homeworkGIT {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Hello Endava");
+        System.out.println("Hello Beko");
+
+    }
 }
