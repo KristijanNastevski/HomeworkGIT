cd591bbfb19b40b5dc704bdb8951800114f69065
Kristijan Nastevski <Kristijan.Nastevski@endava.com>
2019-07-08 12:58:23 +0200

    Commited 3rd changes

--- GitHomework/src/homeworkGIT.java
+++ GitHomework/src/homeworkGIT.java
@@ -2,9 +2,9 @@ public class homeworkGIT {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World - Commited 2nd change");
-        System.out.println("Hello Endava - Commited 2nd change");
-        System.out.println("Hello Beko - Commited 2nd change");
+        System.out.println("Hello World - Commited 3rd change");
+        System.out.println("Hello Endava - Commited 3rd change");
+        System.out.println("Hello Beko - Commited 3rd change");
 
     }
 }
