e7b6fa80f0090fe8865279ced7968381b9d0c2d9
Kristijan Nastevski <Kristijan.Nastevski@endava.com>
2019-07-08 13:45:50 +0200

    Updating the last BAD commit

--- GitHomework/src/homeworkGIT.java
+++ GitHomework/src/homeworkGIT.java
@@ -2,21 +2,15 @@ public class homeworkGIT {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         System.out.println("Hello World - Commited 2nd secondBranch change");
         System.out.println("Hello Endava - Commited 2nd secondBranch change");
         System.out.println("Hello Beko - Commited 2nd secondBranch change");
-=======
+
         System.out.println("Hello World - Commited 1st thirdBranch change");
         System.out.println("Hello Endava - Commited 1st thirdBranch change");
         System.out.println("Hello Beko - Commited 1st thirdBranch change");
 >>>>>>> First thirdBranch change
-=======
-        System.out.println("Hello World - Commited 1st thirdBranch change");
-        System.out.println("Hello Endava - Commited 1st thirdBranch change");
-        System.out.println("Hello Beko - Commited 1st thirdBranch change");
->>>>>>> a85c444e9e945dd346a0f096c150cab5fe75148a
 
     }
 }
