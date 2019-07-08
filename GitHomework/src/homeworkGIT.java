6e540a682cfd7331bad69ee6c66da172c66a79dd
Kristijan Nastevski <Kristijan.Nastevski@endava.com>
2019-07-08 13:18:23 +0200

    First thirdBranch change

--- GitHomework/src/homeworkGIT.java
+++ GitHomework/src/homeworkGIT.java
@@ -2,9 +2,15 @@ public class homeworkGIT {
 
     public static void main(String[] args) {
 
+<<<<<<< HEAD
         System.out.println("Hello World - Commited 2nd secondBranch change");
         System.out.println("Hello Endava - Commited 2nd secondBranch change");
         System.out.println("Hello Beko - Commited 2nd secondBranch change");
+=======
+        System.out.println("Hello World - Commited 1st thirdBranch change");
+        System.out.println("Hello Endava - Commited 1st thirdBranch change");
+        System.out.println("Hello Beko - Commited 1st thirdBranch change");
+>>>>>>> First thirdBranch change
 
     }
 }
