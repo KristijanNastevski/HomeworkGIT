d191379280eb36df57f2371c46304fb145a9c6ce
Kristijan Nastevski <Kristijan.Nastevski@endava.com>
2019-07-08 13:00:59 +0200

    Commited 1st branch changes

--- GitHomework/src/homeworkGIT.java
+++ GitHomework/src/homeworkGIT.java
@@ -2,9 +2,9 @@ public class homeworkGIT {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World - Commited 3rd change");
-        System.out.println("Hello Endava - Commited 3rd change");
-        System.out.println("Hello Beko - Commited 3rd change");
+        System.out.println("Hello World - Commited 1st branch change");
+        System.out.println("Hello Endava - Commited 1st branch change");
+        System.out.println("Hello Beko - Commited 1st branch change");
 
     }
 }
